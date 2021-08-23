f2340bce4d39f1f36d6d6a147ab98d81a8a72d48
chorusg <78986667+chorusg@users.noreply.github.com>
2021-08-23 21:28:19 +0700

    Add files via upload

--- XTT-TokenTimeLock.sol
+++ XTT-TokenTimeLock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity 0.7.6;
 
 import "./SafeERC20.sol";
 
@@ -29,7 +29,11 @@ contract TokenTimelock {
 
     constructor (IERC20 token_, address beneficiary_, uint256 releaseTime_) {
         // solhint-disable-next-line not-rely-on-time
-        require(releaseTime_ > block.timestamp, "TokenTimelock: release time is before current time");
+        require(
+            releaseTime_ > block.timestamp,
+            "TokenTimelock: release time is before current time"
+        );
+        require(beneficiary_ != address(0), "beneficiary_: beneficiary is the zero address");
         _token = token_;
         _beneficiary = beneficiary_;
         _unlocker = beneficiary_;
@@ -67,32 +71,80 @@ contract TokenTimelock {
     /**
      *@notice Extend Lock Time.
      */
-    function extendLockTime(uint256 newReleaseTime_) public virtual {
+     
+    //Declare an Event
+    event ExtendedLockTime(
+        address indexed from,
+        uint256 indexed oldValue,
+        uint256 indexed newValue
+    );
+    
+    function extendLockTime(uint256 newReleaseTime_) external virtual {
         require(_unlocker == msg.sender, "Ownable: caller is not the current unlocker");
-        require(newReleaseTime_ > releaseTime(), "TokenTimelock: new release time can't be before the current release time");
-        require(newReleaseTime_ <= releaseTime() + 365 days, "TokenTimelock: new release time can't be longer than the current release time + 365 days");
+        
+        require(
+            newReleaseTime_ > releaseTime(),
+            "TokenTimelock: new release time can't be before the current release time"
+        );
+        
+        require(
+            newReleaseTime_ <= releaseTime() + 365 days,
+            "TokenTimelock: new release time can't be longer than the current release time + 365 days"
+        );
+        uint256 oldValue = _releaseTime;
         _releaseTime = newReleaseTime_;
+        
+        //Emit an event
+        emit ExtendedLockTime(msg.sender, oldValue, _releaseTime);
     }
     
     /**
      *@notice Set new unlocker address.
      */
-    function setUnlocker(address newUnlocker_) public virtual {
-        require(_unlocker == msg.sender, "Ownable: caller is not the current unlocker");
+    
+     //Declare an Event
+    event SetNewUnlocker(
+        address indexed from,
+        address indexed oldUnlocker,
+        address indexed newUnlocker
+    );
+    
+    function setUnlocker(address newUnlocker_) external virtual {
+        require(
+            _unlocker == msg.sender,
+            "Ownable: caller is not the current unlocker"
+        );
+        
+        require(
+            newUnlocker_ != address(0),
+            "newUnlocker_: new UnLocker is the zero address"
+        );
+        
+        address oldUnlocker = _unlocker;
         _unlocker = newUnlocker_;
+        
+        //Emit an event
+        emit SetNewUnlocker(msg.sender, oldUnlocker, _unlocker);
     }
 
     /**
      * @notice Transfers tokens held by timelock to beneficiary.
      */
-    function release(uint256 releasedValue_) public virtual {
+    function release(uint256 releasedValue_) external virtual {
         // solhint-disable-next-line not-rely-on-time
-        require(block.timestamp >= releaseTime(), "TokenTimelock: current time is before release time");
+        require(
+            block.timestamp >= releaseTime(),
+            "TokenTimelock: current time is before release time"
+        );
 
         uint256 amount = releasedValue_;//token().balanceOf(address(this));
         require(amount > 0, "TokenTimelock: no tokens to release");
 
-        require(unlocker() == msg.sender, "Ownable: caller is not the current unlocker");
+        require(
+            unlocker() == msg.sender,
+            "Ownable: caller is not the current unlocker"
+        );
+        
         token().safeTransfer(beneficiary(), amount);
     }
 }
